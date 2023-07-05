e0da8d713f959b2252336f25aa40f8871001a69b
BananaApache <daniel.miami2005@gmail.com>
2023-07-04 22:32:45 -0400

    dan

---
+++ website_data/static/smooth_scroll.js
@@ -0,0 +1,8 @@
+// document.querySelector('a[href^="#"').forEach((anchor) => {
+//   anchor.addEventListener("click", function (e) {
+//     e.preventDefault();
+//     document
+//       .querySelector(this.getAttribute("href"))
+//       .scrollIntoView({ behavior: "smooth" });
+//   });
+// });
--- website_data/views.py
+++ website_data/views.py
@@ -38,8 +38,16 @@ def form(request):
         form = CreateUserForm(request.POST, request.FILES)
 
         if form.is_valid():
-            print(request.FILES)
             cd = form.cleaned_data
+
+            user_files = {
+                "pdf_id": request.FILES['pdf_id'],
+                "pdf_file1": request.FILES['pdf_file1'],
+                "pdf_file2": "",
+                "pdf_file3": "",
+                "pdf_file4": "",
+                "pdf_file5": "",
+            }
 
             f = CreateUserModel(
                 firstname=cd["firstname"],
@@ -53,7 +61,7 @@ def form(request):
                 birthday=cd["birthday"],
                 school_name=cd["school_name"],
                 school_address=cd["school_address"],
-                pdf_id=request.FILES["pdf_id"],
+                pdf_id=user_files["pdf_id"],
                 parent_firstname=cd["parent_firstname"],
                 parent_lastname=cd["parent_lastname"],
                 parent_email=cd["parent_email"],
@@ -62,27 +70,27 @@ def form(request):
                 category1=cd["category1"],
                 title1=cd["title1"],
                 word_count1=cd["word_count1"],
-                pdf_file1=request.FILES["pdf_file1"],
+                pdf_file1=user_files["pdf_file1"],
 
                 category2=cd["category2"],
                 title2=cd["title2"],
                 word_count2=cd["word_count2"],
-                pdf_file2=request.FILES["pdf_file2"],
+                pdf_file2=user_files["pdf_file2"],
 
                 category3=cd["category3"],
                 title3=cd["title3"],
                 word_count3=cd["word_count3"],
-                pdf_file3=request.FILES["pdf_file3"],
+                pdf_file3=user_files["pdf_file3"],
 
                 category4=cd["category4"],
                 title4=cd["title4"],
                 word_count4=cd["word_count4"],
-                pdf_file4=request.FILES["pdf_file4"],
+                pdf_file4=user_files["pdf_file4"],
 
                 category5=cd["category5"],
                 title5=cd["title5"],
                 word_count5=cd["word_count5"],
-                pdf_file5=request.FILES["pdf_file5"],
+                pdf_file5=user_files["pdf_file5"],
             )
 
             f.save()
