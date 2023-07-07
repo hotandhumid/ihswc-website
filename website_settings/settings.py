bb58308569e884b45d490c33e502461f11f657ca
BananaApache <daniel.miami2005@gmail.com>
2023-07-06 22:08:42 -0400

    dan

--- website_settings/settings.py
+++ website_settings/settings.py
@@ -76,8 +76,13 @@ WSGI_APPLICATION = 'website_settings.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        #'ENGINE': 'django.db.backends.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'railway',
+        'USER': 'postgres',
+        'PASSWORD': 'StDl3RPd0qdyOELStcIa',
+        'HOST': 'containers-us-west-209.railway.app',
+        'PORT': '6015',
     }
 }
 
