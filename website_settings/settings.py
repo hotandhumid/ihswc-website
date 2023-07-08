c621acf4efd0d92ad6a97e68ffa83624914c2b79
BananaApache <daniel.miami2005@gmail.com>
2023-07-07 22:12:57 -0400

    dan

--- website_settings/settings.py
+++ website_settings/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import dj_database_url
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -74,18 +76,20 @@ WSGI_APPLICATION = 'website_settings.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         #'ENGINE': 'django.db.backends.sqlite3',
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#         'NAME': 'highschoolwritingcontest',
+#         'USER': 'highschoolwritingcontest_user',
+#         'PASSWORD': 'Weyd4ife877vMKgLpX4zJsDcbMHFgI6a',
+#         'HOST': 'dpg-cijna3p8g3nc2gamv6d0-a',
+#         'PORT': '5432',
+#     }
+# }
 DATABASES = {
-    'default': {
-        #'ENGINE': 'django.db.backends.sqlite3',
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'railway',
-        'USER': 'postgres',
-        'PASSWORD': 'StDl3RPd0qdyOELStcIa',
-        'HOST': 'containers-us-west-209.railway.app',
-        'PORT': '6015',
-    }
+    "default": dj_database_url.parse(os.environ.get("DATABASE_URL"))
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
