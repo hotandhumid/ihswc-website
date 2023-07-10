c5a09b7114e4b1dcf7a69fcccada10d4e5a0f909
BananaApache <daniel.miami2005@gmail.com>
2023-07-10 12:02:55 -0400

    dan

--- website_data/tests.py
+++ website_data/tests.py
@@ -1,3 +1,5 @@
 from django.test import TestCase
 
 # Create your tests here.
+
+# PGPASSWORD=Weyd4ife877vMKgLpX4zJsDcbMHFgI6a psql -h dpg-cijna3p8g3nc2gamv6d0-a.oregon-postgres.render.com -U highschoolwritingcontest_user highschoolwritingcontest
