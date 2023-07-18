cce03ad2391257fa257c194cdf9bb42741535732
BananaApache <daniel.miami2005@gmail.com>
2023-07-18 00:01:17 -0400

    dan

--- website_data/tests.py
+++ website_data/tests.py
@@ -3,3 +3,11 @@ from django.test import TestCase
 # Create your tests here.
 
 # PGPASSWORD=Weyd4ife877vMKgLpX4zJsDcbMHFgI6a psql -h dpg-cijna3p8g3nc2gamv6d0-a.oregon-postgres.render.com -U highschoolwritingcontest_user highschoolwritingcontest
+
+# put nav in bar, make it sticky
+# put logo on header
+# make solid color background
+# gray bar for nav
+# make it look like duke
+# get headshot for kai, testimony,
+# add bar, with stats (140+ entries), 17+ awards given, 5 different countries
