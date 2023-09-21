2ca50e039d7b8d2372fed5da5209557cfe86d19c
BananaApache <daniel.miami2005@gmail.com>
2023-09-21 10:15:43 -0400

    dan

--- website_settings/urls.py
+++ website_settings/urls.py
@@ -15,10 +15,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls import url
+from django.views.generic import RedirectView
 from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include("website_data.urls"))
+    path('', include("website_data.urls")),
+    url(r'^favicon\.ico$', RedirectView.as_view(url='/static/favicon.ico')),
 ]  + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
