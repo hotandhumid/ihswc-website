f9dc2cfd5b9f3f3168fcc28c28bd31ff1d48ad6c
BananaApache <daniel.miami2005@gmail.com>
2023-10-06 22:24:24 -0400

    dan

--- website_settings/urls.py
+++ website_settings/urls.py
@@ -20,7 +20,7 @@ from django.views.generic import RedirectView
 from django.conf import settings
 from django.conf.urls.static import static
 from django.views.generic.base import TemplateView
-from django.views.static import serve 
+from django.views.static import serve
 
 urlpatterns = [
     path('admin/', admin.site.urls),
