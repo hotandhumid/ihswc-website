8656ebccafadfd68dc6194ee45975513daceeb79
heyixian44 <ahe@prounlimited.com>
2024-03-17 00:53:06 -0400

    Update models

--- website_data/models.py
+++ website_data/models.py
@@ -7,10 +7,10 @@ from django.db import models
 
 class JudgeModel1(models.Model):
     graded_by = models.CharField(max_length=50)
-    review = models.CharField(max_length=300)
+    review = models.CharField(max_length=300, default='', null=True, blank=True)
     rating = models.CharField(max_length=5)
-    submission = models.CharField(max_length=5)
-    sub_number = models.CharField(max_length=5)
+    submission = models.CharField(max_length=5, default='', null=True, blank=True)
+    sub_number = models.CharField(max_length=5, default='', null=True, blank=True)
 
     def get_values(self):
         return {
@@ -22,12 +22,14 @@ class JudgeModel1(models.Model):
     
     def __str__(self):
         return f"Submission {self.submission} graded by {self.graded_by}"
+
+
 class JudgeModel2(models.Model):
     graded_by = models.CharField(max_length=50)
-    review = models.CharField(max_length=300)
+    review = models.CharField(max_length=300, default='', null=True, blank=True)
     rating = models.CharField(max_length=5)
-    submission = models.CharField(max_length=5)
-    sub_number = models.CharField(max_length=5)
+    submission = models.CharField(max_length=5, default='', null=True, blank=True)
+    sub_number = models.CharField(max_length=5, default='', null=True, blank=True)
 
     def get_values(self):
         return {
@@ -36,15 +38,17 @@ class JudgeModel2(models.Model):
             "rating": self.rating, 
             "submission": self.submission
         }
-    
+
     def __str__(self):
         return f"Submission {self.submission} graded by {self.graded_by}"
+
+
 class JudgeModel3(models.Model):
     graded_by = models.CharField(max_length=50)
-    review = models.CharField(max_length=300)
+    review = models.CharField(max_length=300, default='', null=True, blank=True)
     rating = models.CharField(max_length=5)
-    submission = models.CharField(max_length=5)
-    sub_number = models.CharField(max_length=5)
+    submission = models.CharField(max_length=5, default='', null=True, blank=True)
+    sub_number = models.CharField(max_length=5, default='', null=True, blank=True)
 
     def get_values(self):
         return {
@@ -53,51 +57,126 @@ class JudgeModel3(models.Model):
             "rating": self.rating, 
             "submission": self.submission
         }
-    
+
     def __str__(self):
         return f"Submission {self.submission} graded by {self.graded_by}"
 
+
 class TestFileModel(models.Model):
-    firstname = models.CharField(default="___", max_length=100)
-    lastname = models.CharField(default="___", max_length=100)
-    email = models.CharField(default="___", max_length=100)
-    phone_number = models.CharField(default="___", max_length=100)
-    country = models.CharField(default="___", max_length=100)
-    city = models.CharField(default="___", max_length=100)
-    zipcode = models.CharField(default="___", max_length=100)
-    grade_level = models.CharField(default="___", max_length=100)
-    birthday = models.DateField(default="1990-01-01")
-    school_name = models.CharField(default="___", max_length=100)
-    school_address = models.CharField(default="___", max_length=100)
-    parent_firstname = models.CharField(default="___", max_length=100)
-    parent_lastname = models.CharField(default="___", max_length=100)
-    parent_email = models.CharField(default="___", max_length=100)
-    parent_phone_number = models.CharField(default="___", max_length=100)
+    firstname = models.CharField(default="___", max_length=100, blank=True)
+    lastname = models.CharField(default="___", max_length=100, blank=True)
+    email = models.CharField(default="___", max_length=100, blank=True)
+    phone_number = models.CharField(default="___", max_length=100, blank=True)
+    country = models.CharField(default="___", max_length=100, blank=True)
+    city = models.CharField(default="___", max_length=100, blank=True)
+    zipcode = models.CharField(default="___", max_length=100, blank=True)
+    grade_level = models.CharField(default="___", max_length=100, blank=True)
+    birthday = models.DateField(default="1990-01-01", blank=True)
+    school_name = models.CharField(default="___", max_length=100, blank=True)
+    school_address = models.CharField(default="___", max_length=100, blank=True)
+    parent_firstname = models.CharField(default="___", max_length=100, blank=True)
+    parent_lastname = models.CharField(default="___", max_length=100, blank=True)
+    parent_email = models.CharField(default="___", max_length=100, blank=True)
+    parent_phone_number = models.CharField(default="___", max_length=100, blank=True)
 
-    category1 = models.CharField(default="___", max_length=100)
-    title1 = models.CharField(default="___", max_length=100)
-    word_count1 = models.CharField(default="___", max_length=100)
-    pdf_file1 = models.FileField(default="___")
+    category1 = models.CharField(default="___", max_length=100, blank=True)
+    title1 = models.CharField(default="___", max_length=100, blank=True)
+    word_count1 = models.CharField(default="___", max_length=100, blank=True)
+    pdf_file1 = models.FileField(default="___", blank=True)
 
-    category2 = models.CharField(default="___", max_length=100)
-    title2 = models.CharField(default="___", max_length=100)
-    word_count2 = models.CharField(default="___", max_length=100)
-    pdf_file2 = models.FileField(default="___")
+    category2 = models.CharField(default="___", max_length=100, blank=True)
+    title2 = models.CharField(default="___", max_length=100, blank=True)
+    word_count2 = models.CharField(default="___", max_length=100, blank=True)
+    pdf_file2 = models.FileField(default="___", blank=True)
 
-    category3 = models.CharField(default="___", max_length=100)
-    title3 = models.CharField(default="___", max_length=100)
-    word_count3 = models.CharField(default="___", max_length=100)
-    pdf_file3 = models.FileField(default="___")
+    category3 = models.CharField(default="___", max_length=100, blank=True)
+    title3 = models.CharField(default="___", max_length=100, blank=True)
+    word_count3 = models.CharField(default="___", max_length=100, blank=True)
+    pdf_file3 = models.FileField(default="___", blank=True)
 
-    category4 = models.CharField(default="___", max_length=100)
-    title4 = models.CharField(default="___", max_length=100)
-    word_count4 = models.CharField(default="___", max_length=100)
-    pdf_file4 = models.FileField(default="___")
+    category4 = models.CharField(default="___", max_length=100, blank=True)
+    title4 = models.CharField(default="___", max_length=100, blank=True)
+    word_count4 = models.CharField(default="___", max_length=100, blank=True)
+    pdf_file4 = models.FileField(default="___", blank=True)
 
-    category5 = models.CharField(default="___", max_length=100)
-    title5 = models.CharField(default="___", max_length=100)
-    word_count5 = models.CharField(default="___", max_length=100)
-    pdf_file5 = models.FileField(default="___")
+    category5 = models.CharField(default="___", max_length=100, blank=True)
+    title5 = models.CharField(default="___", max_length=100, blank=True)
+    word_count5 = models.CharField(default="___", max_length=100, blank=True)
+    pdf_file5 = models.FileField(default="___", blank=True)
+
+    judge_model1 = models.OneToOneField(
+        JudgeModel1, related_name="test_file_model", on_delete=models.SET_NULL, null=True, blank=True, default=None
+    )
+
+    judge_model2 = models.OneToOneField(
+        JudgeModel2, related_name="test_file_model", on_delete=models.SET_NULL, null=True, blank=True, default=None
+    )
+
+    judge_model3 = models.OneToOneField(
+        JudgeModel3, related_name="test_file_model", on_delete=models.SET_NULL, null=True, blank=True, default=None
+    )
+
+    average_rating = models.FloatField(null=True, blank=True, default=None)     # Field to store average rating
+    all_judges_filled = models.BooleanField(default=False)
+    is_email_notified = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=True)
+    AWARD_CATEGORIES = (
+        ('gold', 'Gold'),
+        ('silver', 'Silver'),
+        ('bronze', 'Bronze'),
+        ('honor', 'Honor'),
+        ('no_award', 'No Award'),
+    )
+
+    # New field for award category
+    award = models.CharField(
+        max_length=10,
+        choices=AWARD_CATEGORIES,
+        default='no_award',
+        blank=True,
+        help_text="Select the award category"
+    )
+
+    def calculate_average_rating(self):
+        ratings = []
+        for judge_model in [self.judge_model1, self.judge_model2, self.judge_model3]:
+            if judge_model and judge_model.rating:
+                try:
+                    ratings.append(float(judge_model.rating))
+                except ValueError:
+                    continue
+
+        if ratings:
+            return sum(ratings) / len(ratings)
+        else:
+            return None
+
+    def save(self, *args, **kwargs):
+        self.average_rating = self.calculate_average_rating()
+
+        # Check if all three judge fields are filled
+        self.all_judges_filled = all([self.judge_model1, self.judge_model2, self.judge_model3])
+
+        super().save(*args, **kwargs)
+
+        # Count of non-none PDF fields
+        pdf_fields = ['pdf_file1', 'pdf_file2', 'pdf_file3', 'pdf_file4', 'pdf_file5']
+        non_none_pdf_count = sum(1 for field in pdf_fields if getattr(self, field, None))
+
+        # Update related JudgeModel instances if they exist
+        for judge_model in [self.judge_model1, self.judge_model2, self.judge_model3]:
+            if judge_model:
+                judge_model.submission = str(self.id)
+                judge_model.sub_number = str(non_none_pdf_count)
+                judge_model.save()  # Save the changes to the JudgeModel instance
+
+    @property
+    def non_empty_judge_fields_count(self):
+        count = 0
+        for judge_field in [self.judge_model1, self.judge_model2, self.judge_model3]:
+            if judge_field is not None:
+                count += 1
+        return count
 
     def get_values(self):
         return {
@@ -141,6 +220,10 @@ class TestFileModel(models.Model):
                 "title5": self.title5,
                 "word_count5": self.word_count5,
                 "pdf_file5": self.pdf_file5,
+                "average_rating": self.average_rating,
+                "all_judges_filled": self.all_judges_filled,
+                "is_email_notified": self.is_email_notified,
+                "is_active": self.is_active,
         }
 
     def __str__(self):
@@ -149,3 +232,11 @@ class TestFileModel(models.Model):
     class Meta:
         verbose_name = "Student Submission"
 
+
+class GeneralSettings(models.Model):
+    key = models.CharField(max_length=255, unique=True, help_text="Unique name of the setting.")
+    value = models.BooleanField(default=True, help_text="Value of the setting.")
+    description = models.TextField(blank=True, help_text="Description of the setting.")
+
+    def __str__(self):
+        return f"{self.key}: {self.value}"
